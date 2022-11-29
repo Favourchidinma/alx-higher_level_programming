1b453dd634805198609422814b09e4164443468f
alexUd01 <alexanderikpeama@gmail.com>
2022-11-30 00:54:04 +0100

    Task 2

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 
 for i in range(97, 123):
-    if i not in (ord('e'), ord('q')):
+    if i != ord('e') and i != ord('q'):  # if i not in (ord('e'), ord('q'))
         print("{}".format(chr(i)), end='')
