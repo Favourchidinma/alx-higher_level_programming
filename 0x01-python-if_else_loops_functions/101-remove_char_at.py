0689a859da1e855236923c1b855b69234e2097d3
alexUd01 <alexanderikpeama@gmail.com>
2022-11-30 04:37:40 +0100

    Task 15

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def remove_char_at(str, n):
+    i = 0
+    while i < len(str):
+        if i == n:
+            i = i + 1
+            continue
+        else:
+            print("{}".format(str[i]), end='')
+            i = i + 1
+    print()
