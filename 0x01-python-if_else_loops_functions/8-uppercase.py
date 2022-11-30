7b1e820ec9ea3ecbc21bb9f808f23d473af8098b
alexUd01 <alexanderikpeama@gmail.com>
2022-11-30 02:12:19 +0100

    Task 8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -5,7 +5,7 @@ def uppercase(str):
     for let in str:
         asc = ord(let)
         if asc >= ord('a') and asc <= ord('z'):
-            print("{}{}".format(chr(asc - 32), '' if n != 0 else '\n'), end='')
+            print("{}{}".format(chr(asc - 32), '' if n > 0 else '\n'), end='')
         else:
-            print("{}{}".format(let, '' if n != 0 else '\n'), end='')
+            print("{}{}".format(let, '' if n > 0 else '\n'), end='')
         n = n - 1
