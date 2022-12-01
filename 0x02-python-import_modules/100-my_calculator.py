0d6fbfc910c60a48f33d0e208e3a707008b3a2c1
alexUd01 <alexanderikpeama@gmail.com>
2022-12-01 22:32:01 +0100

    Task 100

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -6,13 +6,12 @@ if __name__ == "__main__":
 
     ac = len(sys.argv) - 1
     if ac != 3:
-        sys.stderr.write("Usage: ./100-my_calculator.py <a> <operator> <b>\n")
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
 
     operator = sys.argv[2]
     if operator not in ('+', '-', '*', '/'):
-        sys.stderr.write("Unknown operator. Available operators: "
-                         "+, -, * and /\n")
+        print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
 
     a = float(sys.argv[1]) if '.' in sys.argv[1] else int(sys.argv[1])
