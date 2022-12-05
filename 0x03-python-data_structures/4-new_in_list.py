f88331f5bf7a979265093951ee7006a9cd573437
alexUd01 <alexanderikpeama@gmail.com>
2022-12-05 22:17:11 +0100

    Task 4

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,7 +2,9 @@
 
 def new_in_list(my_list, idx, element):
     if my_list is None:
-        return
+        new_list = []
+        new_list.append(element)
+        return new_list
     new_list = my_list
     if idx < 0 or idx >= len(my_list):
         return new_list
