a8266e428efd8f757f12338241dc730640ac6e36
alexUd01 <alexanderikpeama@gmail.com>
2022-12-05 22:39:16 +0100

    Task 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,5 +3,5 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for j in row:
-            print("{:d}".format(j), end='' if row.index(j) == row[-1] else ' ')
+            print("{:d}".format(j), end='' if j == row[-1] else ' ')
         print()
