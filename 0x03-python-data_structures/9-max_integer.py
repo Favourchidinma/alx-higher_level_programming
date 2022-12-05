6a0a0511b048e5434340911f9dd22ea24826316e
alexUd01 <alexanderikpeama@gmail.com>
2022-12-05 22:58:12 +0100

    Task 9

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def max_integer(my_list=[]):
+    if my_list is None or len(my_list) == 0:
+        return 0
+    if len(my_list) != 0:
+        max = my_list[0]
+        for num in my_list:
+            if num > max:
+                max = num
+        return max
