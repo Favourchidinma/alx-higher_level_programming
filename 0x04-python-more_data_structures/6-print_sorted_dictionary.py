1f8c8d98fd94e979b151f37f62b3657ac1c8f430
alexUd01 <alexanderikpeama@gmail.com>
2022-12-07 19:34:08 +0100

    task 8

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 
 def print_sorted_dictionary(a_dictionary):
-    if len(a_dictionary) == 0:
-        return
     for key, value in sorted(a_dictionary.items()):
         print("{}: {}".format(key, value))
