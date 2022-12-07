c2f83fcbe355f585bb668d2162fb3ca6560a0147
alexUd01 <alexanderikpeama@gmail.com>
2022-12-07 23:43:46 +0100

    task 13

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+def weight_average(my_list=[]):
+    if len(my_list) == 0:
+        return 0
+
+    lst1 = [k * v for k, v in my_list]
+    lst2 = [v for k, v in my_list]
+    print(lst1)
+    print(lst2)
+
+    sum_lst1 = 0
+    sum_lst2 = 0
+    for item1, item2 in zip(lst1, lst2):
+        sum_lst1 = sum_lst1 + item1
+        sum_lst2 = sum_lst2 + item2
+    return sum_lst1/sum_lst2
