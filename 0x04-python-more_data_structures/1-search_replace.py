8ad6f996e2ccb45a25e41cc50917fbd84a0feb72
alexUd01 <alexanderikpeama@gmail.com>
2022-12-07 20:59:40 +0100

    correction of no 1

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -4,9 +4,4 @@ def search_replace(my_list, search, replace):
     if my_list == None or len(my_list) == 0:
         return
     new = [replace if item == search else item for item in my_list]
-#    for item in my_list:
-#        if item == search:
-#            new.append(replace)
-#        else:
-#            new.append(item)
     return new
