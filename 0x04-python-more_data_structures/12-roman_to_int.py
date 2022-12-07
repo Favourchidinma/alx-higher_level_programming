265f5bee79d4cc15d96b65312a20d91331b4e9e9
alexUd01 <alexanderikpeama@gmail.com>
2022-12-07 23:28:25 +0100

    task 12: interview prep

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,40 +1,41 @@
 #!/usr/bin/python3
-
-def get_value(str):
-    rom_table = [
-        {"M": 1000, "m": 1000, "CM": 900, "cm": 900},
-        {"D": 500, "d": 500, "CD": 400, "cd": 400},
-        {"C": 100, "c": 100, "XC": 90, "xc": 90},
-        {"L": 50, "l": 50, "XL": 40, "xl": 40},
-        {"X": 10, "x": 10, "IX": 9, "ix": 9},
-        {"V": 5, "v": 5, "IV": 4, "iv": 4},
-        {"I": 1, "i": 1}
-    ]
-    for row in rom_table:
-        if str in row:
-            return row[str]
-    return -1
-
-
 def roman_to_int(roman_string):
-    if roman_string is None:
+    try:
+        roman_string = roman_string.upper()
+    except AttributeError:
         return 0
-    if not roman_string.isalpha():
-        return 0
+    else:
+        num = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
+    res = 0
+    skip = False
 
-    stat = None
-    result = 0
+    end_index = len(roman_string) - 1
 
-    i = 0
-    while i < (len(roman_string)):
-        if (stat := get_value(roman_string[i:i+2])) != -1:
-            result = result + stat
-            i = i + 2
+    for i in range(end_index, -1, -1):
+        if skip:
+            skip = False
             continue
-        if (stat := get_value(roman_string[i])) != -1:
-            result = result + stat
-            i = i + 1
-            continue
+
+        if roman_string[i] in num.keys():
+            curr = num[roman_string[i]]
+            next = num[roman_string[i - 1]]
+
+            if i == 0:
+                next = 1000000
+
+            if curr > next:
+                res += (curr - next)
+                skip = True
+            else:
+                res += curr
         else:
             return 0
-    return result
+    return res
