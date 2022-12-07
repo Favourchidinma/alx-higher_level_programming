432ca8caf3458aaf6b9fe06f9fa60c721f061795
alexUd01 <alexanderikpeama@gmail.com>
2022-12-07 21:08:46 +0100

    correction of no 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def best_score(a_dictionary):
-    if a_dictionary is None:
+    if a_dictionary is None or len(a_dictionary) == 0:
         return a_dictionary
 
     val_list = list(a_dictionary.values())
