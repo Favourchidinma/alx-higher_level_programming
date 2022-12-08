914e363fe8c6571185d39deebf3f00e10055cf1c
alexUd01 <alexanderikpeama@gmail.com>
2022-12-08 01:13:39 +0100

    task 15

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -4,10 +4,9 @@ def complex_delete(a_dictionary, value):
     if value not in a_dictionary.values():
         return a_dictionary
 
-    new = {}
-    for k, v in a_dictionary.items():
+    new = a_dictionary.copy()
+    for k, v in new.items():
         if v == value:
-            continue
-        new[k] = v
-    a_dictionary = new
+            del a_dictionary[k]
+
     return a_dictionary
