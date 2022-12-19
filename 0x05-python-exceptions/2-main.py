6f37e27283a7a13f2f02be49ab8f4056c0a0ad4b
alexUd01 <alexanderikpeama@gmail.com>
2022-12-19 22:24:51 +0100

    task 2: correction

--- 0x05-python-exceptions/2-main.py
+++ 0x05-python-exceptions/2-main.py
@@ -5,7 +5,7 @@ safe_print_list_integers = \
 
 my_list = [1, 2, 3, 4, 5]
 
-nb_print = safe_print_list_integers(my_list, -3)
+nb_print = safe_print_list_integers(None, 2)
 print("nb_print: {:d}".format(nb_print))
 
 my_list = [1, 2, None, 3, "School", 4, 5]
