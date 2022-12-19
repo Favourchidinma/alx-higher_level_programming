f0bea312ff3dfd78211c58aa9869bcf9d45fa0d3
alexUd01 <alexanderikpeama@gmail.com>
2022-12-19 22:54:40 +0100

    task 100: correction

--- 0x05-python-exceptions/100-main.py
+++ 0x05-python-exceptions/100-main.py
@@ -12,14 +12,13 @@ value = -89
 has_been_print = safe_print_integer_err(value)
 if not has_been_print:
     print("{} is not an integer".format(value))
-    
+
 value = 'School'
 has_been_print = safe_print_integer_err(value)
 if not has_been_print:
     print("{} is not an integer".format(value))
-    
+
 value = None
 has_been_print = safe_print_integer_err(value)
 if not has_been_print:
     print("{} is not an integer".format(value))
-    
