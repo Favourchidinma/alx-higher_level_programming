667eb7051a87a5fbcbc419c5a4c9cfe2624820e7
alexUd01 <alexanderikpeama@gmail.com>
2023-01-09 23:03:55 +0100

    init

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -5,6 +5,7 @@ A module that contains one function that returns a list of available
 attributes in a module
 """
 
+
 def lookup(obj):
     """A function that returns list of available attributes"""
     return list(dir(obj))
