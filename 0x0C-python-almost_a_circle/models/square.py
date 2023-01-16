e13ec978e71a73cd0ca580f25ede52f466339f6c
alexUd01 <alexanderikpeama@gmail.com>
2023-01-16 19:20:17 +0100

    tests

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -8,9 +8,13 @@ from models.rectangle import Rectangle
 class Square(Rectangle):
 
     def __init__(self, size, x=0, y=0, id=None):
+        """A function that initializes attributes.
+        """
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
+        """Returns a formated string.
+        """
         return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y,
                                                  self.width)
 
